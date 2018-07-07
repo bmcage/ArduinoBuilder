09ebdb3caf57b59fbd9f4b7da2345c08d3123188
Maria-Cristina Ciocci <mcristina.ciocci@gmail.com>
2018-07-07 20:02:03 +0200

    convert to Blockly4Arduino server

--- config.js
+++ config.js
@@ -4,12 +4,12 @@ var config = {};
 config.port = 7000;
 
 // Path to the local Arduino directory where you can add libraries for the projects
-config.localArduinoPath = '/home/robin/Arduino';
+config.localArduinoPath = '/home/mcciocci/Arduino';
 
 // Path to the arduino installation directory
-config.builderPath = '/home/robin/arduino-1.8.5';
+config.builderPath = '/home/mcciocci/arduino-1.8.5';
 
 // Path where you want to compile/build the sketches (tmp directory)
-config.localBuildPath = '/home/robin/zzzArduinoBuilder';
+config.localBuildPath = '/home/mcciocci/zzzArduinoBuilder';
 
 module.exports = config;
